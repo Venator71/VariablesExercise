d197cdf876efcfc7047aa0c980df0d7372a13e48
Venator71 <b.johnhunter71@gmail.com>
2022-03-01 16:13:19 -0600

    First commit

---
+++ script.js
@@ -0,0 +1,56 @@
+// Q 1
+
+// const
+
+// Q 2
+
+let num = 20;
+
+// Q 3
+
+num = 15;
+
+// Q 4
+
+num = num + 7;
+
+
+// Q  5
+
+num--;
+
+//  Q 6
+
+num = "This is not a number";
+
+//  Q 7 
+
+let blank
+
+// Q 8 
+console.log(blank);
+
+
+// Q 9
+blank = null
+
+// console.log(blank);
+
+// Bonus
+
+// B 1
+const  quarter = " 0.25", dime = " 0.10", nickel = " 0.05", penny = " 0.01";
+
+// B 2
+
+console.log(quarter + dime + nickel + penny);
+
+// B 3
+
+let letters = "qwertyuiop";
+
+// B 4
+console.log(letters.length);
+console.log(letters[9]);
+
+// I Like JavaScript 
